a577b67e2cc00197ead4d1291b793cb4fc884c22
1000zoo <cjswldn99@gmail.com>
2026-03-07 21:31:54 +0900

    chore: 허용 테이블(strategy_timeframe) 추가

--- src/common/db/util.py
+++ src/common/db/util.py
@@ -1,5 +1,6 @@
 import json
 from typing import Any, Mapping, List, Tuple
+
 from sqlalchemy import text
 
 from src.common.db.connection import SessionLocal
@@ -26,6 +27,7 @@ ALLOWED_TABLES = {
     'scheduler',
     'regime_state',
     'strategy',
+    'strategy_timeframe',
 }
 
 def strip_none(d: Mapping[str, Any],) -> dict[str, Any]:
