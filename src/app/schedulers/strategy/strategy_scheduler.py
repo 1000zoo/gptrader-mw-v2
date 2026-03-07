aec02e3f851dd840b44a820aa5f265fef2c61a04
1000zoo <cjswldn99@gmail.com>
2026-03-07 22:25:04 +0900

    feat: account 관련 usecase

--- src/app/schedulers/strategy/strategy_scheduler.py
+++ src/app/schedulers/strategy/strategy_scheduler.py
@@ -1,8 +1,11 @@
 from loguru import logger
 
-from src.app.usecase.market_data_preparation_usecase import MarketDataPreparationUseCase
-from src.binance.service.trader.account_service import AccountService
+from src.app.usecase.account_praparation_usecase import AccountPreparationUseCase, JobPosition
+from src.app.usecase.market_data_preparation_usecase import MarketDataPreparationUseCase, PreparedDataDto, \
+    PrepareDataDto
+from src.app.usecase.strategy_preparation_usecase import StrategyPreparationUseCase
 from src.ops.service.scheduler.scheduler_service import SchedulerService
+from src.strategy.strategies.IStrategy import IStrategy
 
 
 class StrategyScheduler:
@@ -10,33 +13,27 @@ class StrategyScheduler:
         self.name = self.__class__.__name__
 
         self.scheduler_service = SchedulerService()
-        self.account_service = AccountService()
+        self.account_usecase = AccountPreparationUseCase()
+        self.strategy_usecase = StrategyPreparationUseCase()
         self.market_data_usecase = MarketDataPreparationUseCase()
 
     def _can_start_exit_scheduler(self):
         if not self.scheduler_service.is_valid_scheduler(self.name):
             logger.info(f"{self.name} state is not Y")
             return False
-        if not self.account_service.has_position():
-            logger.info(f"has no open position now.. skip strategy")
-            return False
         return True
 
-    def _get_prepare_data(self):
-        position = self.account_service.get_positions()
-
-    def run_exit(self):
+    async def run_exit(self):
         if not self._can_start_exit_scheduler():
             return
+        job_position: JobPosition = await self.account_usecase.get_current_job_position()
+        job = job_position.job
+        position = job_position.position
 
-        data = self.market_data_usecase()
+        strategy: IStrategy = await self.strategy_usecase.load_strategy_by_priority()
 
+        prepared_data: PreparedDataDto = await self.market_data_usecase.prepare_data(
+            PrepareDataDto(
 
-
-
-
-
-
-
-
-
+            )
+        )
