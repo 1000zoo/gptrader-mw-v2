24283377d8b806308243202a3f0e5858fd7e35fe
1000zoo <cjswldn99@gmail.com>
2026-03-20 23:03:42 +0900

    refactor: 불필요 codex 생성 테스트 제거 및 confest 수정

--- tests/test_class_loader.py
+++
@@ -1,6 +0,0 @@
-from src.common.util.class_loader import *
-from src.strategy.strategies.IStrategy import IStrategy
-
-
-def test_load_class():
-    s = load_class('src.strategy.strategies.volatility_breakout_regime_strategy', 'VolatilityBreakoutRegimeStrategy', IStrategy)
